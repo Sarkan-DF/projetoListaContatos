3462fef868828582f3135c1559e9df9b06ad0eca
Igor Matos <sarkan.igor@gmail.com>
2023-01-09 22:31:01 -0300

    LocalStoreg e DOM

--- contatos.js
+++ contatos.js
@@ -1,4 +1,11 @@
-let listaContatos = []
+
+function salvarLocalStorage() {
+    localStorage.setItem("contatos", JSON.stringify(getStorage));
+}
+
+const getStorage = JSON.parse(localStorage.getItem("contatos")) || [{ id: 99, nome: "moises", telefone: "9000" },];
+
+console.log(getStorage);
 
 function criaUsuario() {
     console.clear();
@@ -14,45 +21,50 @@ function criaUsuario() {
     let telefoneUsuario = prompt("Digite um telefone: ")
     usuario.telefone = telefoneUsuario
     
-    const validaTelefone = listaContatos.some((valor, indice, array) => telefoneUsuario === valor.telefone)
+    const validaTelefone = getStorage.some((valor, indice, array) => telefoneUsuario === valor.telefone)
 
     if(validaTelefone){
         alert("Numero de telefone já cadastrado!")
         return
     }
 
-    listaContatos.push(usuario)
+    getStorage.push(usuario)
+    salvarLocalStorage();
     mostraLista();
 }
 
 function mostraLista() {
-    console.clear();
-    for (const dado of listaContatos) {
-        console.log(`Id: ${dado.id}, Nome: ${dado.nome}, Telefone: ${dado.telefone} \n`)
-    }
+    const divShow = document.getElementById("conteiner")
+    divShow.innerText = ""
+
+    getStorage.map((usuario) => {
+        divShow.innerHTML += `<br>Nome: ${usuario.nome} - Telefone: ${usuario.telefone} - Id: ${usuario.id} <br>`
+    })
 }
 
 function alteraUsuario() {
      const idAltera = Number(prompt("Digite o id da usuaria que deseja alterar: "))
-     const validaId = listaContatos.some((valor, indice, array) => idAltera === valor.id )
+     const validaId = getStorage.some((valor, indice, array) => idAltera === valor.id )
      if(validaId) {
         const alteraNome = prompt("Digite o novo nome: ")
         const alteraTelefone = prompt("Digite o novo telefone: ")
-        const indiceUsuario = listaContatos.find((valor, indice, array) => idAltera === valor.id)
+        const indiceUsuario = getStorage.find((valor, indice, array) => idAltera === valor.id)
         indiceUsuario.nome = alteraNome
         indiceUsuario.telefone = alteraTelefone
     }
+    salvarLocalStorage();
     mostraLista();
 }
 
 function deletaUsuario() {
     const idApagar = Number(prompt("Digite o id do usuario que deseja apagar: "))
-    const validaId = listaContatos.some((valor, indice, array) => idApagar === valor.id )
+    const validaId = getStorage.some((valor, indice, array) => idApagar === valor.id )
     if (validaId){
-        const indiceApagar = listaContatos.findIndex((valor, indice, array) => idApagar === valor.id )
-        listaContatos.splice(indiceApagar,1)
-        mostraLista();
+        const indiceApagar = getStorage.findIndex((valor, indice, array) => idApagar === valor.id )
+        getStorage.splice(indiceApagar,1)
+        salvarLocalStorage();
     }else {
         return alert("Item não existente!")
     }
+    mostraLista();
 }
